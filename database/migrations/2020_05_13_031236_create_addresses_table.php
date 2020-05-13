cc9e51957b25d2821622d19b7efeda38ad06f3ed
ridhoajibx <ridhoajibx@gmail.com>
2020-05-13 11:00:48 +0700

    relation one to one with cascade on delete

--- database/migrations/2020_05_13_031236_create_addresses_table.php
+++ database/migrations/2020_05_13_031236_create_addresses_table.php
@@ -17,6 +17,7 @@ class CreateAddressesTable extends Migration
             $table->id();
             $table->foreignId('user_id');
             $table->string('country');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
