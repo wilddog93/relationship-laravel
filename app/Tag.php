b58d63f3e4a518ab5b5c91846a222a893aa3a668
ridhoajibx <ridhoajibx@gmail.com>
2020-05-14 17:08:43 +0700

    membuat uniq key menggunakan slug

--- app/Tag.php
+++ app/Tag.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+    public function getRouteKeyName()
+    {
+        return 'slug';   
+    }
+
     public function posts()
     {
         return $this->belongsToMany(Post::class);
